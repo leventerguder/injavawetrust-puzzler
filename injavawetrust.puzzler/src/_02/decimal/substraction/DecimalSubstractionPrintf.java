3ad988564633a1face30e0f628c67a4454b9a942
leventerguder <erguder.levent@gmail.com>
2016-07-23 22:58:52 +0300

    Monetary Calculation

---
+++ injavawetrust.puzzler/src/_02/decimal/substraction/DecimalSubstractionPrintf.java
@@ -0,0 +1,17 @@
+package _02.decimal.substraction;
+
+public class DecimalSubstractionPrintf {
+
+	public static void main(String[] args) {
+
+		System.out.printf("%.2f\n", 2.00 - 1.1);
+		System.out.printf("%.2f\n", 2.00 - 1.2);
+		System.out.printf("%.2f\n", 2.00 - 1.3);
+		System.out.printf("%.2f\n", 2.00 - 1.4);
+		System.out.printf("%.2f\n", 2.00 - 1.5);
+		System.out.printf("%.2f\n", 2.00 - 1.6);
+		System.out.printf("%.2f\n", 2.00 - 1.7);
+		System.out.printf("%.2f\n", 2.00 - 1.8);
+		System.out.printf("%.2f\n", 2.00 - 1.9);
+	}
+}
