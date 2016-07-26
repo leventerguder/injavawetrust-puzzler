6082ef7224013b0fe84fad009b966cd468521119
leventerguder <erguder.levent@gmail.com>
2016-07-26 22:24:38 +0300

    ternary

---
+++ injavawetrust.puzzler/src/_08/ternary/TernaryDarkCorner.java
@@ -0,0 +1,12 @@
+package _08.ternary;
+
+public class TernaryDarkCorner {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		char x = 'X';
+		int i = 0;
+
+		System.out.print(true ? x : 0);
+		System.out.print(false ? i : x);
+	}
+}
