141d5b92ca44f93691cf1b83c0e305cb0bec06f6
levent.erguder <erguder.levent@gmail.com>
2016-08-10 17:13:02 +0300

    commit

---
+++ injavawetrust.puzzler/src/_11/char$/array/CharArrayExample.java
@@ -0,0 +1,11 @@
+package _11.char$.array;
+
+public class CharArrayExample {
+	public static void main(String[] args) {
+
+		char[] numbers = { '1', '2', '3' };
+		System.out.println(numbers);
+
+		System.out.println("Java " + numbers);
+	}
+}
