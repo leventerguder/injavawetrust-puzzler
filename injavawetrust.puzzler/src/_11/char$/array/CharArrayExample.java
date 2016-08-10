347e062e211894c1c7ff4e019ff06cf8f77f6c5f
leventerguder <erguder.levent@gmail.com>
2016-08-10 21:15:12 +0300

    char array

--- injavawetrust.puzzler/src/_11/char$/array/CharArrayExample.java
+++ injavawetrust.puzzler/src/_11/char$/array/CharArrayExample.java
@@ -5,14 +5,20 @@ public class CharArrayExample {
 
 		char[] numbers = { '1', '2', '3' };
 		System.out.println(numbers);
-
+		
+		//public void println(String x) 
 		System.out.println("Java " + numbers);
 
 		System.out.println("fix : ");
 		
 		System.out.println("Java " + String.valueOf(numbers));
 		
+ 
 		System.out.print("Java ");
 		System.out.println(numbers);
+		
+		// public void println(Object x) 
+		Object numbers2 = new char[]{ '1', '2', '3' };
+		System.out.println(numbers2);
 	}
 }
