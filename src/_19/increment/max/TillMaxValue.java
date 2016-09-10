9f987a8070206bdd9194f833f37e5362c5c9b901
levent <levent@ekmekTeknesi.ekmekTeknesi>
2016-09-10 13:56:17 +0300

    increment Integer.MAX_VALUE

---
+++ src/_19/increment/max/TillMaxValue.java
@@ -0,0 +1,17 @@
+package _19.increment.max;
+
+public class TillMaxValue {
+	public static final int END = Integer.MAX_VALUE;
+	public static final int START = END - 100;
+
+	public static void main(String[] args) {
+
+		int count = 0;
+
+		for (int i = START; i <= END; i++)
+			count++;
+		System.out.println(count);
+
+	}
+
+}
---
+++ src/_19/increment/max/TillMaxValueFixed.java
@@ -0,0 +1,16 @@
+package _19.increment.max;
+
+public class TillMaxValueFixed {
+	public static final int END = Integer.MAX_VALUE;
+	public static final int START = END - 100;
+
+	public static void main(String[] args) {
+		int count = 0;
+
+		//use long i intead of int i
+		for (long i = START; i <= END; i++)
+			count++;
+		System.out.println(count);
+
+	}
+}
