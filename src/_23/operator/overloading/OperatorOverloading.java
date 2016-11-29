6d8b2a5fe3dd58b82f532d2fae322f403b5736d9
levent.erguder <erguder.levent@gmail.com>
2016-11-29 21:29:36 +0200

    operator overloading

---
+++ src/_23/operator/overloading/OperatorOverloading.java
@@ -0,0 +1,13 @@
+package _23.operator.overloading;
+
+public class OperatorOverloading {
+
+	public static void main(String[] args) {
+
+		String i = "Java!";
+
+		while (i != i + 0) {
+			System.out.println(" + operator overloading...");
+		}
+	}
+}
